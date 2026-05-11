5f419f58e31bdb4a633be2549a217b4c1590c7da
Priyanka Debnath <priyanka.neub@gmail.com>
2026-05-12 02:06:03 +0600

    update

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -16,6 +16,7 @@ const ShopContextProvider = (props) => {
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
     const [products, setProducts] = useState([]);
+    const [token, setToken] = useState('');
     const navigate = useNavigate();
     const addToCart = async (itemId, size) => {
         if (!size) {
@@ -103,7 +104,7 @@ const ShopContextProvider = (props) => {
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount, navigate
+        getCartAmount, navigate, token, setToken
 
     }
     return (
